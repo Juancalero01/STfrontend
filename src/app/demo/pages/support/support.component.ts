e67031cc2b7ea615e3f542fa9964dc8cd3d5e9b4
juancalero <marcelocalero5@gmail.com>
2023-10-25 08:43:32 -0300

    push

--- src/app/demo/pages/support/support.component.ts
+++ src/app/demo/pages/support/support.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
-  styleUrls: [],
 })
 export class SupportComponent {}
