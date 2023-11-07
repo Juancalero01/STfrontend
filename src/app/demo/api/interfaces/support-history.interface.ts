e6619186a7a31fa8cfce6f9d9a68b33cfcd5c3f4
juancalero <marcelocalero5@gmail.com>
2023-11-07 12:00:11 -0300

    push

--- src/app/demo/api/interfaces/support-history.interface.ts
+++ src/app/demo/api/interfaces/support-history.interface.ts
@@ -7,4 +7,5 @@ export interface ISupportHistory {
   stateNext: ISupportState;
   remarks: string;
   service: ISupport;
+  user: number;
 }
--- src/app/demo/pages/support/components/form-history/support-form-history.component.ts
+++ src/app/demo/pages/support/components/form-history/support-form-history.component.ts
@@ -21,9 +21,11 @@ export class SupportFormHistoryComponent {
   ) {}
 
   public supportHistoryForm: FormGroup = this.buildForm();
-  public supportStatesDropdown: ISupportState[] = [];
+  public currentStates: ISupportState[] = [];
+  public nextStates: ISupportState[] = [];
 
   public ngOnInit(): void {
+    this.setDefaultFormData();
     this.loadStates();
     this.loadForm(this.config.data);
   }
@@ -35,31 +37,76 @@ export class SupportFormHistoryComponent {
       service: data,
     });
   }
+
   private loadStates(): void {
     this.supportStateService.findAll().subscribe({
-      next: (supportStates: ISupportState[]) => {
-        this.supportStatesDropdown = supportStates;
-      },
-      error: (e: any) => {
-        if (e.status === 0) {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Error de conexión con el servidor',
-          });
+      next: (states: ISupportState[]) => {
+        this.currentStates = states;
+        this.nextStates = states;
+
+        const stateCurrentValue =
+          this.supportHistoryForm.get('stateCurrent')?.value;
+        let optionsToShow = 1;
+
+        if (stateCurrentValue === 2) {
+          optionsToShow = 3;
+        } else if (stateCurrentValue === 3) {
+          optionsToShow = 2;
         } else {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error al cargar los estados',
-            detail: 'Error al cargar los estados',
-          });
+          optionsToShow = 1;
         }
+
+        this.nextStates = this.nextStates.filter((state) => {
+          return (
+            state.id !== stateCurrentValue &&
+            state.id > stateCurrentValue &&
+            optionsToShow-- > 0
+          );
+        });
+
+        // const stateCurrentValue =
+        //   this.supportHistoryForm.get('stateCurrent')?.value;
+        // let stateNextValue = stateCurrentValue;
+
+        // let optionsToShow = 1;
+
+        // if (stateCurrentValue === 2) {
+        //   optionsToShow = 3;
+        // } else if (stateCurrentValue === 3) {
+        //   optionsToShow = 2;
+        // } else {
+        //   optionsToShow = 1;
+        // }
+
+        // this.nextStates = this.nextStates.filter((state) => {
+        //   if (state.id === stateCurrentValue) {
+        //     return false;
+        //   }
+
+        //   if (state.id <= stateNextValue) {
+        //     return false;
+        //   }
+
+        //   if (optionsToShow > 0) {
+        //     optionsToShow--;
+        //     return true;
+        //   }
+
+        //   return false;
+        // });
       },
     });
   }
 
+  private setDefaultFormData(): void {
+    this.supportHistoryForm
+      .get('dateEntry')
+      ?.setValue(new Date().toISOString().split('T')[0]);
+  }
+
   private buildForm(): FormGroup {
     return this.formBuilder.group({
+      dateEntry: [{ value: null, disabled: true }],
       reclaim: [{ value: null, disabled: true }],
       stateCurrent: [{ value: null, disabled: true }],
       stateNext: [null, [Validators.required]],
@@ -81,5 +128,11 @@ export class SupportFormHistoryComponent {
         'p-button-rounded p-button-text p-button-sm font-medium p-button-secondary',
       accept: () => this.ref.close(),
     });
+  }
+
+  public saveForm(): void {
+    const formValue = this.supportHistoryForm.value;
+    formValue.stateCurrent = this.supportHistoryForm.get('stateCurrent')?.value;
+    (formValue.user = 1), console.log(formValue);
   }
 }
