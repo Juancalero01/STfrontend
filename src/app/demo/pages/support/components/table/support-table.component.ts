70b36ed971c51c4ba2787e8a18c39c6acb674b39
juancalero <marcelocalero5@gmail.com>
2023-11-10 12:32:15 -0300

    push

--- src/app/demo/pages/support/components/table/support-table.component.ts
+++ src/app/demo/pages/support/components/table/support-table.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { MessageService } from 'primeng/api';
 import { ISupport } from 'src/app/demo/api/interfaces/support.interface';
 import { SupportFormComponent } from '../form/support-form.component';
 import { SupportService } from 'src/app/demo/api/services/support.service';
@@ -13,8 +12,7 @@ import { Table } from 'primeng/table';
 export class SupportTableComponent {
   constructor(
     private readonly supportService: SupportService,
-    private readonly dialogService: DialogService,
-    private readonly messageService: MessageService
+    private readonly dialogService: DialogService
   ) {}
 
   public supportData: ISupport[] = [];
@@ -38,49 +36,18 @@ export class SupportTableComponent {
 
   private loadSupports(): void {
     this.supportService.findAll().subscribe({
-      next: (supports: ISupport[]) => {
-        this.supportData = supports.sort(
+      next: (supports: ISupport[]) =>
+        (this.supportData = supports.sort(
           (a, b) => a.priority.id - b.priority.id
-        );
-      },
-      error: (e: any) => {
-        if (e.status === 0) {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Error de conexión con el servidor',
-          });
-        } else {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Error al cargar los soportes',
-          });
-        }
-      },
+        )),
     });
   }
 
-  public createSupport() {
+  public openSupportForm(support?: ISupport) {
     this.ref = this.dialogService.open(SupportFormComponent, {
-      header: 'FORMULARIO DE REGISTRO DE SOPORTE TÉCNICO',
-      width: '100vw',
-      style: { 'min-width': '100vw', 'min-height': '100vh' },
-      closable: false,
-      closeOnEscape: false,
-      dismissableMask: false,
-      showHeader: true,
-      position: 'center',
-    });
-
-    this.ref.onClose.subscribe(() => {
-      this.loadSupports();
-    });
-  }
-
-  public updateSupport(support: ISupport): void {
-    this.ref = this.dialogService.open(SupportFormComponent, {
-      header: 'FORMULARIO DE ACTUALIZACIÓN DE SOPORTE TÉCNICO',
+      header: support
+        ? 'FORMULARIO DE ACTUALIZACIÓN DE SOPORTE TÉCNICO'
+        : 'FORMULARIO DE REGISTRO DE SOPORTE TÉCNICO',
       width: '100vw',
       style: { 'min-width': '100vw', 'min-height': '100vh' },
       closable: false,
@@ -90,30 +57,13 @@ export class SupportTableComponent {
       position: 'center',
       data: support,
     });
-
-    this.ref.onClose.subscribe(() => {
-      this.loadSupports();
-    });
-  }
-
-  public getPercentage(support: ISupport): number {
-    const percentages: any = {
-      1: 0,
-      2: 20,
-      3: 30,
-      4: 30,
-      5: 50,
-      6: 90,
-      7: 99,
-      8: 0,
-      9: 0,
-    };
-
-    return percentages[support.state.id];
+    this.ref.onClose.subscribe(() => this.loadSupports());
   }
 
   public cleanFilters(table: Table, filter: any) {
     table.clear();
     filter.value = '';
   }
+
+  // proximanente una funcionalidad para que pinte dependiendo los dias que van pasando desde la fecha de creacion y la propiedad de vencimiento de la prioridad
 }
