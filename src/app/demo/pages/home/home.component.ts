755c43bec39000007f7c3ff15e7dbae6259b030a
juancalero <marcelocalero5@gmail.com>
2023-12-05 11:59:36 -0300

    push

--- src/app/demo/pages/home/home.component.ts
+++ src/app/demo/pages/home/home.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: ` <p>Proximamente</p> `,
+  template: ` <p>Proximamente dashboard en general</p> `,
 })
 export class HomeComponent {}
