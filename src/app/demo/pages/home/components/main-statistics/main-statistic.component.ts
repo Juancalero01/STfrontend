8219d070618703bd55dea0edf6266f330317295b
juancalero <marcelocalero5@gmail.com>
2023-12-18 23:35:47 -0300

    push

---
+++ src/app/demo/pages/home/components/main-statistics/main-statistic.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-statistic',
+  templateUrl: './main-statistic.component.html',
+  styles: [],
+})
+export class MainStatisticComponent {}
--- src/app/demo/pages/home/home.module.ts
+++ src/app/demo/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MainStatisticComponent } from './components/main-statistics/main-statistic.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainStatisticComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
