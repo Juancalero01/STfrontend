d7ce5c19f5ff1fc085bc30d1aad49cccd6f2f874
juancalero <marcelocalero5@gmail.com>
2023-12-21 11:56:42 -0300

    push

--- src/app/demo/pages/indicator/indicator.component.ts
+++ src/app/demo/pages/indicator/indicator.component.ts
@@ -3,6 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-indicator',
   templateUrl: './indicator.component.html',
-  styles: [],
 })
-export class IndicatorComponent {}
+export class IndicatorComponent {
+  data: any;
+
+  options: any;
+
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    this.data = {
+      labels: ['TEST A', 'TEST B', 'TEST C'],
+      datasets: [
+        {
+          data: [540, 325, 702],
+          backgroundColor: [
+            documentStyle.getPropertyValue('--cyan-700'),
+            documentStyle.getPropertyValue('--cyan-500'),
+            documentStyle.getPropertyValue('--cyan-200'),
+          ],
+        },
+      ],
+    };
+
+    this.options = {
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+            color: textColor,
+          },
+        },
+        title: {
+          display: true,
+          text: 'Custom Chart Test',
+        },
+      },
+    };
+  }
+}
