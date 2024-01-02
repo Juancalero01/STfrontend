a9be0fd9f519967d39542b0060af878659af4533
juancalero <marcelocalero5@gmail.com>
2024-01-02 11:29:18 -0300

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,7 @@
-import { Component, HostListener } from '@angular/core';
-import { TokenService } from './demo/api/services/token.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: ` <router-outlet />`,
 })
-export class AppComponent {
-  constructor(private authService: TokenService) {}
-
-  @HostListener('window:beforeunload', ['$event'])
-  onBeforeUnload(event: any) {
-    this.authService.deleteToken();
-  }
-}
+export class AppComponent {}
