525ee364ad8ec9f69e227746eb6202a127af227f
juancalero <marcelocalero5@gmail.com>
2024-01-24 18:51:19 -0300

    push

--- src/app/demo/pages/support-history/components/table/support-history-table.component.ts
+++ src/app/demo/pages/support-history/components/table/support-history-table.component.ts
@@ -6,6 +6,7 @@ import { SupportService } from 'src/app/demo/api/services/support.service';
 import { SupportHistoryFormComponent } from '../form/support-history-form.component';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-support-history-table',
@@ -16,13 +17,15 @@ export class SupportHistoryTableComponent {
     private readonly supportService: SupportService,
     private readonly dialogService: DialogService,
     private readonly formBuilder: FormBuilder,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private readonly messageService: MessageService
   ) {}
 
   public supports: ISupport[] = [];
   public ref: DynamicDialogRef = new DynamicDialogRef();
   public reclaim!: string;
   public showFilters: boolean = true;
+  public showSearch: boolean = false;
   public historyForm: FormGroup = this.buildForm();
 
   public ngOnInit(): void {
@@ -31,15 +34,55 @@ export class SupportHistoryTableComponent {
     });
 
     if (this.reclaim) {
-      this.showFilters = false;
       this.searchReclaimService(this.reclaim);
+      this.showFilters = false;
+      this.showSearch = true;
     }
+
+    this.historyForm.valueChanges.subscribe({
+      next: ({ reclaim, serial }) => {
+        const reclaimControl = this.historyForm.get('reclaim');
+        const serialControl = this.historyForm.get('serial');
+        if (reclaim && reclaim.trim() !== '') {
+          if (serialControl?.enabled) {
+            serialControl?.disable({ emitEvent: false });
+            serialControl.removeValidators(Validators.required);
+            serialControl.updateValueAndValidity();
+          }
+        } else {
+          if (serialControl?.disabled) {
+            serialControl?.enable({ emitEvent: false });
+            serialControl.addValidators(Validators.required);
+            serialControl.updateValueAndValidity();
+          }
+        }
+        if (serial && serial.trim() !== '') {
+          if (reclaimControl?.enabled) {
+            reclaimControl?.disable({ emitEvent: false });
+            reclaimControl.removeValidators(Validators.required);
+            reclaimControl.updateValueAndValidity();
+          }
+        } else {
+          if (reclaimControl?.disabled) {
+            reclaimControl?.enable({ emitEvent: false });
+            reclaimControl.addValidators(Validators.required);
+            reclaimControl.updateValueAndValidity();
+          }
+        }
+      },
+    });
   }
 
   private buildForm(): FormGroup {
     return this.formBuilder.group({
-      reclaim: [null],
-      serial: [null],
+      reclaim: [
+        null,
+        [Validators.required, Validators.pattern(/^CNET-\d{8}-\d+$/)],
+      ],
+      serial: [
+        null,
+        [Validators.required, Validators.pattern(/^\d{1,4}(-\d{4,5})?$/)],
+      ],
     });
   }
 
@@ -80,18 +123,26 @@ export class SupportHistoryTableComponent {
   }
 
   public searchHistory() {
-    //Testeo con reclaim
-    // this.searchReclaimService(this.historyForm.get('reclaim')?.value);
-    this.searchSerialProduct(this.historyForm.get('serial')?.value);
-  }
-
-  private getDataForm(): void {
-    return this.historyForm.getRawValue();
+    const reclaimValue = this.historyForm.get('reclaim')?.value;
+    const serialValue = this.historyForm.get('serial')?.value;
+    if (reclaimValue && reclaimValue.trim() !== '') {
+      this.searchReclaimService(reclaimValue);
+    } else if (serialValue && serialValue.trim() !== '') {
+      this.searchSerialProduct(serialValue);
+    }
+    this.showSearch = true;
   }
 
   public searchReclaimService(reclaim: string): void {
     this.supportService.getServiceByReclaim(reclaim).subscribe({
       next: (supports: ISupport[]) => {
+        if (supports.length === 0) {
+          this.messageService.add({
+            severity: 'info',
+            summary: 'Operación',
+            detail: 'Sin resultados',
+          });
+        }
         this.supports = supports;
       },
     });
@@ -100,8 +151,20 @@ export class SupportHistoryTableComponent {
   public searchSerialProduct(serial: string): void {
     this.supportService.getServicesByProductSerial(serial).subscribe({
       next: (supports: ISupport[]) => {
+        if (supports.length === 0) {
+          this.messageService.add({
+            severity: 'info',
+            summary: 'Operación',
+            detail: 'Sin resultados',
+          });
+          this.showSearch = false;
+        }
         this.supports = supports;
       },
     });
+  }
+
+  public getChangesToUpdate(): boolean {
+    return !this.historyForm.pristine;
   }
 }
