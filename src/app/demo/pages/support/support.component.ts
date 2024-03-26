9a1b3749632d050732edffa7722f1ad1b457d76a
juancalero <marcelocalero5@gmail.com>
2024-03-25 22:30:26 -0300

    push

--- src/app/demo/pages/support/support.component.ts
+++ src/app/demo/pages/support/support.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-support',
-  templateUrl: './support.component.html',
+  template: `
+    <section class="h-full flex flex-column gap-4">
+      <app-panel-header
+        title="Gestión de Servicios Técnicos "
+        subtitle="Administra y gestiona los datos de tus ingresos en este panel."
+      >
+      </app-panel-header>
+
+      <app-support-table />
+    </section>
+  `,
 })
 export class SupportComponent {}
